edd68917c29b77e9f516fcecd26976d89a855c4d
Ezra Miller <emiller@lsoc.org>
2015-12-27 17:54:10 -0600

    ok better

--- scripts/js/blackbox.js
+++ scripts/js/blackbox.js
@@ -111,32 +111,6 @@ function blackbox(el, inputImage, origImage, size, cbs) {
         renderer.autoClear = false;
         // renderer.setPixelRatio(1);
         // console.log(window.devicePixelRatio);
-        createEffect();
-
-        document.addEventListener("mousemove", onMouseMove);
-        document.addEventListener("mousedown", onMouseDown);
-        document.addEventListener("mouseup", onMouseUp);
-        document.addEventListener('touchstart', onDocumentTouchStart, false);
-        document.addEventListener('touchdown', onDocumentTouchStart, false);
-        document.addEventListener('touchmove', onDocumentTouchMove, false);
-        document.addEventListener('touchend', onDocumentTouchEnd, false);
-        document.addEventListener('touchcancel', onDocumentTouchEnd, false);
-        document.addEventListener('touchleave', onDocumentTouchEnd, false);
-        // document.addEventListener('keydown', onKeyDown, false);
-        debounceResize = debounce(onWindowResize, 250);
-        window.addEventListener("resize", debounceResize);
-        if(isMobile)div.addEventListener("click", null);
-        if(isMobile)div.addEventListener("touchstart", null);
-        if(isMobile)div.addEventListener("touchdown", null);
-        infoButton.addEventListener("click", cbs.info);
-        infoButton.addEventListener("touchstart", cbs.info);
-        infoButton.addEventListener("touchdown", cbs.info);
-        uploadButton.addEventListener('click', onClickButton)
-        uploadButton.addEventListener('touchstart', onClickButton)
-        uploadButton.addEventListener('touchdown', onClickButton)
-
-        div.appendChild(renderer.domElement)
-        el.appendChild(div)
 
         if(isMobile){
             if(window.innerHeight > window.innerWidth){
@@ -144,6 +118,31 @@ function blackbox(el, inputImage, origImage, size, cbs) {
             } else {
                 overlay.style.display = "none";
                 // animate();
+                createEffect();
+                document.addEventListener("mousemove", onMouseMove);
+                document.addEventListener("mousedown", onMouseDown);
+                document.addEventListener("mouseup", onMouseUp);
+                document.addEventListener('touchstart', onDocumentTouchStart, false);
+                document.addEventListener('touchdown', onDocumentTouchStart, false);
+                document.addEventListener('touchmove', onDocumentTouchMove, false);
+                document.addEventListener('touchend', onDocumentTouchEnd, false);
+                document.addEventListener('touchcancel', onDocumentTouchEnd, false);
+                document.addEventListener('touchleave', onDocumentTouchEnd, false);
+                // document.addEventListener('keydown', onKeyDown, false);
+                debounceResize = debounce(onWindowResize, 250);
+                window.addEventListener("resize", debounceResize);
+                if(isMobile)div.addEventListener("click", null);
+                if(isMobile)div.addEventListener("touchstart", null);
+                if(isMobile)div.addEventListener("touchdown", null);
+                infoButton.addEventListener("click", cbs.info);
+                infoButton.addEventListener("touchstart", cbs.info);
+                infoButton.addEventListener("touchdown", cbs.info);
+                uploadButton.addEventListener('click', onClickButton)
+                uploadButton.addEventListener('touchstart', onClickButton)
+                uploadButton.addEventListener('touchdown', onClickButton)
+
+                div.appendChild(renderer.domElement)
+                el.appendChild(div)
                 onWindowResize();
 
             }
